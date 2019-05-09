45be4e16b2c0198fe56fb757387c0818052c2455
moranja <jeffadammoran@gmail.com>
2019-05-09 16:37:59 -0500

    done

--- test/index-test.js
+++ test/index-test.js
@@ -11,6 +11,8 @@ describe('createStore', function() {
     global.document = jsdom.jsdom('<html><body><div id="container"></div></body></html>');
     candyStore = createStore(candyReducer);
     countStore = createStore(countReducer);
+    candyStore.dispatch({ type: '@@INIT' })
+    countStore.dispatch({ type: '@@INIT' })
   })
 
   it('returns an object', function() {
