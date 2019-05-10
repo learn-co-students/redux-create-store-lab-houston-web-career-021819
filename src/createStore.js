5dd80a74a6938f9868fd6b4dbb6eb52fae368b62
EliLauff <eli.lauffenburger@gmail.com>
2019-05-10 12:05:36 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,26 @@
+import { prependOnceListener } from "cluster";
+
 export default function createStore(reducer) {
   // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  dispatch({ type: "@@INIT" });
+
+  return {
+    dispatch,
+    getState
+  };
 }
 
 function render() {
-  const container = document.getElementById('container');
+  const container = document.getElementById("container");
 }
